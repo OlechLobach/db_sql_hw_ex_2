e35d047848d9da209da8e791ea8ec36e9116193d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-05-11 04:08:38 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE Hospital;
+GO
+
+USE Hospital;
+GO
+
+CREATE TABLE Departments (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Financing MONEY NOT NULL DEFAULT 0 CHECK (Financing >= 0),
+    Floor INT NOT NULL CHECK (Floor >= 1),
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Diseases (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL UNIQUE,
+    Severity INT NOT NULL DEFAULT 1 CHECK (Severity >= 1)
+);
+
+CREATE TABLE Doctors (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(MAX) NOT NULL,
+    Phone CHAR(10) NOT NULL,
+    Premium MONEY NOT NULL DEFAULT 0 CHECK (Premium >= 0),
+    Salary MONEY NOT NULL CHECK (Salary > 0),
+    Surname NVARCHAR(MAX) NOT NULL
+);
+
+CREATE TABLE Examinations (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    DayOfWeek INT NOT NULL CHECK (DayOfWeek BETWEEN 1 AND 7),
+    EndTime TIME NOT NULL,
+    Name NVARCHAR(100) NOT NULL UNIQUE,
+    StartTime TIME NOT NULL
+);
+
+CREATE TABLE Wards (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Floor INT NOT NULL CHECK (Floor >= 1),
+    Name NVARCHAR(20) NOT NULL UNIQUE
+);
